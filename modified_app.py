6f8dc4779dfc186c391834dbe4e303090a18e7f4
sarinasabharwal19 <79310094+sarinasabharwal19@users.noreply.github.com>
2023-11-26 11:55:35 -0500

    Update modified_app.py

--- modified_app.py
+++ modified_app.py
@@ -5,18 +5,19 @@ import streamlit as st
 import time
 from PIL import Image
 
+
+
 client = OpenAI(api_key=os.environ['OPEN_API_KEY'])
 assistant_id = os.environ['ASSIS_ID']
-
 if "start_chat" not in st.session_state:
     st.session_state.start_chat = False
 
 if "thread_id" not in st.session_state:
     st.session_state.thread_id = None
 
+
 st.set_page_config(page_title="NSCLC Insights", page_icon=":speech_balloon:")
 
-# Start chat button in the sidebar
 if st.sidebar.button("Start Chat"):
     st.session_state.start_chat = True
     thread = client.beta.threads.create()
@@ -26,7 +27,7 @@ if st.sidebar.button("Start Chat"):
 # Main chat interface setup
 st.title("NSCLC Insights")
 
-# Function to process messages (remains unchanged)
+
 def process_message_with_citations(message):
     """Extract content and annotations from the message and format citations as footnotes."""
     
@@ -36,6 +37,73 @@ def process_message_with_citations(message):
     # Add footnotes to the end of the message content
     full_response = message_content.value 
     return full_response
+    
+# Only show the chat interface if the chat has been started
+if st.session_state.start_chat:
+    # Initialize the model and messages list if not already in session state
+    if "openai_model" not in st.session_state:
+        st.session_state.openai_model = "gpt-4-1106-preview"
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display existing messages in the chat
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    # Chat input for the user
+    if prompt := st.chat_input("What insights would you like to unlock?"):
+        # Add user message to the state and display it
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Add the user's message to the existing thread
+        client.beta.threads.messages.create(
+            thread_id=st.session_state.thread_id,
+            role="user",
+            content=prompt
+        )
+
+        # Create a run with additional instructions
+        run = client.beta.threads.runs.create(thread_id=st.session_state.thread_id,
+                                              assistant_id=assistant_id)
+
+        # Poll for the run to complete and retrieve the assistant's messages
+
+
+        while True:
+            run = client.beta.threads.runs.retrieve(thread_id=st.session_state.thread_id, run_id=run.id)
+
+            if run.status=="completed":
+                messages = client.beta.threads.messages.list(thread_id=st.session_state.thread_id)
+                print(messages)
+                break;
+
+        # Retrieve messages added by the assistant
+        latest_message = messages.data[0]
+
+        for content in latest_message.content:
+                if hasattr(content, 'image_file'):
+                  a=content.image_file.file_id
+                  api_response = client.files.with_raw_response.retrieve_content(a)
+                  if api_response.status_code == 200:
+                    content2 = api_response.content
+                  with open('image.png', 'wb') as f:
+                    f.write(content2)
+                  print('File downloaded successfully.')
+                  print(a)
+                  im = Image.open('image.png')
+                  st.image(im)
+        for content in latest_message.content:
+            if hasattr(content, 'text'):
+                full_response=content.text.value
+                st.session_state.messages.append({"role": "assistant", "content": full_response})
+                with st.chat_message("assistant"):
+                    st.markdown(full_response, unsafe_allow_html=True)
+                break;
+else:
+    # Prompt to start the chat
+    st.write("Please click 'Start Chat' to begin the conversation.")
 
 # Custom CSS for styling
 st.markdown(
@@ -64,22 +132,5 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Chat display area
-with st.container():
-    chat_history = st.empty()
-    
-    # Display chat history
-    # ... existing code ...
-
-# User input
-with st.container():
-    col1, col2 = st.columns([5, 1])
-    with col1:
-        user_input = st.text_input("Your message", key='user_input')
-    with col2:
-        if st.button('Send'):
-            # Process and update chat window (you'll need to implement the logic)
-            
-            st.session_state.chat_window += f"\nUser: {user_input}\nBot: [response]"
-
-# Add more sections as needed
+# Redesigning the chat interface while retaining original functionality
+# (The chat display and input area will be styled and organized as per the new design)
